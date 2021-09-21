42ff6191d08c7d0dbf85c00b4731afaebd81e7c2
sha-zha <adeline.servan@gmail.com>
2021-09-21 13:28:27 +0200

    tabs and first components

--- App.js
+++ App.js
@@ -1,51 +1,31 @@
-import React, { useEffect, useState } from 'react';
-import {ActivityIndicator, FlatList, ScrollView, Text, View} from 'react-native';
-
-export default App = () => {
-    const [isLoading, setLoading] = useState(true);
-    const [data, setData] = useState([]);
-
-    const getMovies = async () => {
-        try {
-            const response = await fetch('https://reactnative.dev/movies.json');
-            const json = await response.json();
-            setData(json.movies);
-        } catch (error) {
-            console.error(error);
-        } finally {
-            setLoading(false);
-        }
-    }
-
-    useEffect(() => {
-        getMovies();
-    }, []);
-
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from './components/Home';
+function HomeScreen() {
     return (
-        <View style={{ flex: 1, padding: 24 }}>
-            {isLoading ? <ActivityIndicator/> : (
-                <FlatList
-                    data={data}
-                    keyExtractor={({ id }, index) => id}
-                    renderItem={({ item }) => (
-                        <Text>{item.title}, {item.releaseYear}</Text>
-                    )}
-                />
+        <Home/>
+    );
+}
 
-            )}
-
-            <ScrollView>
-                {
-                    data.map(item => (
-
-                            <View key={item.id }>
-                                <Text>
-                                    {item.title }
-                                </Text>
-                            </View>
-                    ))
-                }
-            </ScrollView>
+function SettingsScreen() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Settings!</Text>
         </View>
     );
-};
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator>
+                <Tab.Screen name="Home" component={HomeScreen} />
+                <Tab.Screen name="Settings" component={SettingsScreen} />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
+}
